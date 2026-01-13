c7f9b2ace893c6c41bd80861bd9f8a62a66ac1e8
Nihal7778 <nihal77778@gmail.com>
2026-01-13 15:29:44 -0500

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # End-End-MediRAG-GEN-AI
+
+#1 Create a conda environment 
+
+conda - create -n llmapp python = 3.10 -y
+
+#b
+
+conda activate llmapp
+
+#2 Install the requirements
+
+pip install -r requirement.txt
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mediRAG-GenAI",
+    version="0.1.0",
+    author="Nihal Mallikarjun",
+    author_email="nihal77778@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+
+)
